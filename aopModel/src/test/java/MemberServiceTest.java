6c5ab1c491dcac4c34d95d4a9b0bc16beb554d77
wudr@yuelinghui.com <wudr@yuelinghui.com>
2018-12-06 11:58:46 +0800

    修改

--- aopModel/src/test/java/MemberServiceTest.java
+++ aopModel/src/test/java/MemberServiceTest.java
@@ -2,6 +2,7 @@ import com.yun.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,12 +11,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MemberServiceTest {
 
-    @Autowired
-    MemberService memberService;
+//    @Autowired
+//    MemberService memberService;
 
     @Test
     public void add(){
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("classpath*:appcontext.xml");
 //        memberService.add(null);
+        MemberService memberService = (MemberService) applicationContext.getBean("memberService");
         memberService.delete(1);
+        System.out.println("22222");
     }
 }
